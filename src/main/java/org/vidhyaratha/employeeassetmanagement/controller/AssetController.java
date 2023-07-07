dbdc6df9e3a26e2877f45faa8850d3a5aeb08829
VIDHYA RATHA <vidhyaratha21@gmail.com>
2023-07-07 14:45:29 -0500

    added: schema diagram

--- src/main/java/org/vidhyaratha/employeeassetmanagement/controller/AssetController.java
+++ src/main/java/org/vidhyaratha/employeeassetmanagement/controller/AssetController.java
@@ -5,7 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.expression.AccessException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
