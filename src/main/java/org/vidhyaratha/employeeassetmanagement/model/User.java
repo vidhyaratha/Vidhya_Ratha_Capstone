65104002719c97783793eacd3e52db287f08ffd9
Vidhya Ratha <vidhyaratha21@gmail.com>
2024-03-29 16:59:05 -0500

    changed user table name to employee

--- src/main/java/org/vidhyaratha/employeeassetmanagement/model/User.java
+++ src/main/java/org/vidhyaratha/employeeassetmanagement/model/User.java
@@ -11,7 +11,7 @@ import java.util.Collection;
 import java.util.List;
 
 
-@Table(name = "user")
+@Table(name = "employee")
 @Entity
 @Getter
 @Setter
--- src/main/java/org/vidhyaratha/employeeassetmanagement/repository/UserRepository.java
+++ src/main/java/org/vidhyaratha/employeeassetmanagement/repository/UserRepository.java
@@ -9,7 +9,7 @@ import org.vidhyaratha.employeeassetmanagement.model.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
 
-    @Query(value = "Select * from user where email = :email", nativeQuery = true)
+    @Query(value = "Select * from employee where email = :email", nativeQuery = true)
     public User findUserByEmail(String email);
 
     public User findUserByEmpId(String empId);
